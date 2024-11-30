dc355eb6c41c2444fb933551638afe67d8391d03
XKimYX <dexmareborn0101@gmail.com>
2024-11-30 14:17:49 +0100

    Retour à la version initial

--- Calculateur de remise.js
+++ Calculateur de remise.js
@@ -1,49 +1,5 @@
 const readlineSync = require('readline-sync');
 
-
-function calculerRemiseSurAchat(montant) {
-    const achat = {
-        montant,
-        remise: {
-            pourcentage:0,
-            valeur: 0,
-        },
-        facture: 0,
-    }
-
-    if (achat.montant > 100000) {
-        achat.remise.pourcentage = 15
-        achat.remise.valeur = (montant * achat.remise)/100
-        achat.facture = achat.montant - achat.remise.valeur
-        return achat
-    }
-
-    if (achat.montant > 500000 && achat.montant <= 100000){
-        achat.remise.pourcentage = 10
-        achat.remise.valeur = (montant * achat.remise)/100
-        achat.facture = achat.montant - achat.remise.valeur
-        return achat
-    }
-
-    if (achat.montant > 250000 && achat.montant <= 500000) {
-        achat.remise.pourcentage = 5
-        achat.remise.valeur = (montant * achat.remise)/100
-        achat.facture = achat.montant - achat.remise.valeur
-        return achat
-    }
-
-        achat.remise.pourcentage = 0
-        achat.remise.valeur = 0
-        achat.facture = montant
-        return achat
-}
-
-function afficher(facture){
-        console.log(`Montant initial: ${facture.montant}`);
-        console.log(`Remise: ${facture.remise}%`)
-        console.log(`Montant finale: ${facture.montant - facture.remise}`);
-}
-
 function remiseAchat() {
     console.log("")
     console.log("Le programme s’exécute....")
@@ -88,11 +44,8 @@ function askUser(){
         console.log("")
         console.log("Le programme s’exécute....")
         console.log("")
-        let montant = parseInt(prompt("Veuillez saisie un montant:"))
         console.log("")
-    //    remiseAchat();
-        const facture = calculerRemiseSurAchat(montant)
-        afficher(facture)
+       remiseAchat();
         askUser(); 
     }
     else {
@@ -104,11 +57,4 @@ function askUser(){
     
 }
 
-// askUser()
-
-function main(){
-    
-    askUser()
-}
-
-main()
+askUser()
